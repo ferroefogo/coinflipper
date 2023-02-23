9adab7c987bd81ab96f7ca5e472646d31d4d49fe
Waer <waerdev@gmail.com>
2023-02-23 22:25:10 +0000

    Added Font support for Docker

--- cogs/odds/converter.py
+++ cogs/odds/converter.py
@@ -6,7 +6,7 @@ def converter(number):
     img = Image.new('RGB', (457, 457))
     W,H = img.size
     d = ImageDraw.Draw(img)
-    font = ImageFont.truetype('Arial.ttf', 200)
+    font = ImageFont.truetype('fonts/Arial.ttf', 200)
     _, _, w, h = d.textbbox((0,0), str(number), font=font)
     d.text(((W-w)/2, (H-h)/2), str(number), font=font, fill=(255, 255, 255))
 
